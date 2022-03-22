5eb170773427f37f0ba93ed70d930ed2495de4dd
Shaikimran123 <shaik.immu321@gmail.com>
2022-03-22 12:04:00 +0530

    student_class

---
+++ student.java
@@ -0,0 +1,35 @@
+class student{
+
+int studentid,studentage;
+String studentname,studentfirstname,studentlastname;
+public int getstudentid(){
+return studentid;
+}
+public void setstudentid(int studentid){
+this.studentid=studentid;
+}
+public String getstudentname(){
+return studentname;
+}
+public void setstudentname(String studentname){
+this.studentname=studentname;
+}
+public int getstudentage(){
+return studentage;
+}
+public void setstudentage(int studentage){
+this.studentage=studentage;
+}
+public String getstudentfirstname(){
+return studentfirstname;
+}
+public void setstudentfirstname(String studentfirstname){
+this.studentfirstname=studentfirstname;
+}
+public String getstudentlastname(){
+return studentlastname;
+}
+public void setstudentlastname(String studentlastname){
+this.studentlastname=studentlastname;
+}
+}
