124fde48b07b902f29e0a55c9b54ff0f2c25a17a
Shaikimran123 <shaik.immu321@gmail.com>
2022-03-22 12:05:10 +0530

    student_test

---
+++ test.java
@@ -0,0 +1,15 @@
+class test{
+public static void main(String args[]){
+student ob=new student();
+ob.setstudentid(10);
+ob.setstudentname("imran");
+ob.setstudentage(10);
+ob.setstudentfirstname("shaik");
+ob.setstudentlastname("immu");
+System.out.println(ob.getstudentid());
+System.out.println(ob.getstudentname());
+System.out.println(ob.getstudentage());
+System.out.println(ob.getstudentfirstname());
+System.out.println(ob.getstudentlastname());
+}}
+
