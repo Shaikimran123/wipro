3bb56f8871213e7223edfb2cca7555335cfac144
Shaikimran123 <shaik.immu321@gmail.com>
2022-03-22 11:59:00 +0530

    calculator_class

---
+++ calculator.java
@@ -0,0 +1,26 @@
+class calculator{
+int a,b;
+public int getadd(int a,int b){
+int c=a+b;
+return c;
+}
+
+public int getsub(int a,int b){
+int c=a-b;
+return c;
+}
+
+public int getmul(int a,int b){
+int c=a*b;
+return c;
+}
+
+public int getdiv(int a,int b){
+int c=a/b;
+return c;
+}
+public int getmoddiv(int a,int b){
+int c=a%b;
+return c;
+}
+}
