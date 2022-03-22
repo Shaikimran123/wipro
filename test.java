2ae214a5db78ba49feb9862e7c17c35a02844961
Shaikimran123 <84969992+Shaikimran123@users.noreply.github.com>
2022-03-22 12:06:42 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ public static void main(String args[]){
 student ob=new student();
 ob.setstudentid(10);
 ob.setstudentname("imran");
-ob.setstudentage(10);
+ob.setstudentage(22);
 ob.setstudentfirstname("shaik");
 ob.setstudentlastname("immu");
 System.out.println(ob.getstudentid());
